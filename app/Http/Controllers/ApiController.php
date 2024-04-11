a5a39c738c37bef7a1a077e4a4fa9eafa9a5c302
eiji yoshida <eijiyoshida1912@gmail.com>
2024-04-11 09:04:00 +0900

    都道府県ごとに出せるように実装

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,29 +7,27 @@ use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
-    public function index()
+    public function index($id)
     {
-        $url = "https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001?year=2015&area=13";
+        $url = "https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001?year=2015&area=" . $id;
 
         $response = Http::withHeaders([
             "Ocp-Apim-Subscription-Key" => "3bd5b3c5ba5d4d1d8b04703622709715"
         ])->get($url);
 
-        $str = $response->json();
-
-        var_dump($response->json());
         //アクセスした後データの確認。
         //HTTPステータスに問題がなければそのままデータを返す、
         //問題があれば空の配列を返す。
         if ($response->ok() === TRUE) {
-            return mb_convert_encoding($response->json(), "UTF-8", "auto");
-            // return $response->json();
+            /*
             return response()->json(
                 [],
                 200,
                 [],
                 JSON_UNESCAPED_UNICODE
             );
+            */
+            return $response;
         } else {
             return array();
         }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('get', ApiController::class);
+Route::apiResource('realestate/{id}', ApiController::class);
